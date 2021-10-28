d9e918238f697db127c42eb940e9f44d8bb068ed
Zhang Liangs <42020812+ZhangLiangShan@users.noreply.github.com>
2021-10-28 22:27:48 +0800

    Add files via upload

---
+++ Zhaoxi.CourseManagement/ViewModel/CoursePageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zhaoxi.CourseManagement.Model;
+
+namespace Zhaoxi.CourseManagement.ViewModel
+{
+    public class CoursePageViewModel
+    {
+        public ObservableCollection<CategoryItemModel> CategoryCourses { get; set; }
+        public ObservableCollection<CategoryItemModel> CategoryTechnology{ get; set; }
+        public ObservableCollection<CategoryItemModel> CategoryTeacher{ get; set; }
+
+        public CoursePageViewModel()
+        {
+            this.CategoryCourses = new ObservableCollection<CategoryItemModel>();
+            this.CategoryCourses.Add(new CategoryItemModel("全部", true));
+            this.CategoryCourses.Add(new CategoryItemModel("公开课"));
+            this.CategoryCourses.Add(new CategoryItemModel("VIP课程"));
+
+            this.CategoryTechnology = new ObservableCollection<CategoryItemModel>();
+            this.CategoryTechnology.Add(new CategoryItemModel("全部", true));
+            this.CategoryTechnology.Add(new CategoryItemModel("C#"));
+            this.CategoryTechnology.Add(new CategoryItemModel("ASP.NET"));
+            this.CategoryTechnology.Add(new CategoryItemModel("微服务"));
+            this.CategoryTechnology.Add(new CategoryItemModel("Java"));
+
+            this.CategoryTeacher = new ObservableCollection<CategoryItemModel>();
+            this.CategoryTeacher.Add(new CategoryItemModel("全部", true));
+            this.CategoryTeacher.Add(new CategoryItemModel("Eleven"));
+            this.CategoryTeacher.Add(new CategoryItemModel("Richard"));
+            this.CategoryTeacher.Add(new CategoryItemModel("Clay"));
+        }
+    }
+}
